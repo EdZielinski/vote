06e657bc2a87bea0f2854fecc8924884b07228f7
EdZielinski <edz028@g.harvard.edu>
2018-07-26 12:25:39 -0400

    first message

---
+++ application.py
@@ -0,0 +1,18 @@
+import os
+import requests
+#flask item
+from flask import Flask, jsonify, render_template, request
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+#decorators
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@socketio.on("submit vote")
+def vote(data):
+    selection = data["selection"]
+    emit("announce vote", {"selection": selection}, broadcast=True)
